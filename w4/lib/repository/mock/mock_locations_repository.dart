c89a72925dca497c6551bfa765f6777a9dc49625
Phinnaroth <phinnaroth.phoeun@student.cadt.edu.kh>
2025-03-05 19:40:36 +0700

    BLA-101 – Refactor Location Service

--- w4/lib/repository/mock/mock_locations_repository.dart
+++ w4/lib/repository/mock/mock_locations_repository.dart
@@ -1,15 +1,13 @@
+import 'package:w4/dummy_data/dummy_data.dart';
 import 'package:w4/model/ride/locations.dart';
 import 'package:w4/repository/locations_repository.dart';
 
 class MockLocationsRepository extends LocationsRepository {
-  final Location phnompenh = const Location(name: 'Phnom Penh', country: Country.cambodia);
-  final Location siemreap = const Location(name: 'Siem Reap', country: Country.cambodia);
-  final Location battambang = const Location(name: 'Battambang', country: Country.cambodia);
-  final Location sihanoukville = const Location(name: 'Sihanoukville', country: Country.cambodia);
-  final Location kampot = const Location(name: 'Kampot', country: Country.cambodia);
+  final List<Location> _locations = fakeLocations;
+
   
   @override
   List<Location> getLocations() {
-    return [phnompenh, siemreap, battambang, sihanoukville, kampot];
+    return _locations;
   }
 }
